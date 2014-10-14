6465851c26ede9cb684c863649f3c15b2cbdc808
kar <kar2wlan@gmail.com>
2014-10-15 03:03:00 +0530

    added KernelTweaker to the build

--- config/common.mk
+++ config/common.mk
@@ -138,7 +138,8 @@ PRODUCT_PACKAGES += \
     LockClock \
     CMUpdater \
     CMAccount \
-    CMHome 
+    CMHome \
+    KernelTweaker
 
 # CM Hardware Abstraction Framework
 PRODUCT_PACKAGES += \
